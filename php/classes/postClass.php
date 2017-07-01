7d6bad0d3cf2b4509f8554c22dbf370e51f236d7
sweettirexkiller <piotrjankiewicz02@gmail.com>
2017-07-01 15:43:55 +0200

    started creating and testing post class

--- php/classes/postClass.php
+++ php/classes/postClass.php
@@ -1,3 +1,92 @@
 <?php
+class post{
+    private $id;
+    private $user_id;
+    private $content;
+    private $dateOfCreation;
     
+    public function __construct() {
+        $this->id = NULL;
+        $this->setContent('');
+        $this->setDateOfCreation(null);
+        $this->setUser_id(null);
+    }
+            
+    function getId() {
+        return $this->id;
+    }
+
+    function getUser_id() {
+        return $this->user_id;
+    }
+
+    function getContent() {
+        return $this->content;
+    }
+
+    function getDateOfCreation() {
+        return $this->dateOfCreation;
+    }
+
+    function setUser_id($user_id) {
+        $this->user_id = $user_id;
+    }
+
+    function setContent($content) {
+        $this->content = $content;
+    }
+
+    function setDateOfCreation($dateOfCreation) {
+        $this->dateOfCreation = $dateOfCreation;
+    }
+
+    static public function loadPostById(PDO $conn, $id){
+        $stmt = $conn->prepare('SELECT * FROM post WHERE id=:id');
+        $result = $stmt->execute(['id' => $id]);
+        
+        if($result == true && $stmt->rowCount() > 0){
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            
+            $loadPost = new post();
+            $loadPost->id = $row['id'];
+            $loadPost->content = $row['content'];
+            $loadPost->user_id= $row['user_id'];
+            $loadPost->dateOfCreation= $row['dateOfCreation'];
+            
+            
+            return $loadPost;
+        }
+        
+        return NULL;
+    }
+    
+    static public function loadAllPostsByUserId(PDO $conn, $id){
+        $sql = "SELECT post.user_id, post.id as post_id, post.content, post.dateOfCreation FROM user JOIN post ON user.id=post.user_id WHERE user.id=:id";
+        $ret = [];
+        
+        if($id > 0){
+            $stmt = $conn->prepare($sql);
+            $didSucced = $stmt->execute(['id'=>$id]);
+            
+            $result = $stmt->fetchAll(); 
+            
+            if($didSucced === true){
+                foreach($result as $row){
+                    $loadPost = new post();
+                    $loadPost->id = $row['post_id'];
+                    $loadPost->content = $row['content'];
+                    $loadPost->user_id= $row['user_id'];
+                    $loadPost->dateOfCreation= $row['dateOfCreation'];
+                    
+                    $ret[] = $loadPost;
+                }
+            }
+        }
+        
+        return $ret;
+    }
+    
+    
+    
+}
 
--- php/classes/postTests.php
+++ php/classes/postTests.php
@@ -1,2 +1,14 @@
 <?php
+require_once __DIR__.'/../dbConfig.php';
+require_once __DIR__.'/postClass.php';
+$conn = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS);
 
+//TESTY dla loadPostById
+
+//$testPost = post::loadPostById($conn, 1);
+//var_dump($testPost);
+
+//TESTY dla loadAllPostsByUserId
+
+//$allTestPostsForUserId1 = post::loadAllPostsByUserId($conn, 1);
+//var_dump($allTestPostsForUserId1);
