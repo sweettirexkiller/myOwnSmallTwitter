10804626c88d77940020aa4298e79ed78323f888
sweettirexkiller <piotrjankiewicz02@gmail.com>
2017-07-01 13:27:28 +0200

    changed structure of post table

--- sql/post.sql
+++ sql/post.sql
@@ -5,6 +5,7 @@
 CREATE TABLE IF NOT EXISTS post (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     content VARCHAR(240),
-    user_id INT NOT NULL, 
+    user_id INT NOT NULL,
+    dateOfCreation DATETIME NOT NULL,
     FOREIGN KEY(user_id) REFERENCES user(id)
 );
