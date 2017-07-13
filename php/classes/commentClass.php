f90e761ec544e7cc9703d6d9eed509cde3f81336
sweettirexkiller <piotrjankiewicz02@gmail.com>
2017-07-13 18:07:37 +0200

    loadCommentById method done and tested

--- php/classes/commentClass.php
+++ php/classes/commentClass.php
@@ -1,4 +1,76 @@
 <?php
 class comment {
+    private $id;
+    private $userId;
+    private $postId;
+    private $creation_date;
+    private $content;
     
+    function __construct() {
+        $this->id = NULL;
+        $this->userId = 0;
+        $this->postId = 0;
+        $this->creation_date = 0;
+        $this->content = '';
+    }
+
+    
+    function getId() {
+        return $this->id;
+    }
+
+    function getUserId() {
+        return $this->userId;
+    }
+
+    function getPostId() {
+        return $this->postId;
+    }
+
+    function getCreation_date() {
+        return $this->creation_date;
+    }
+
+    function getText() {
+        return $this->text;
+    }
+    
+
+    function setUserId($userId) {
+        $this->userId = $userId;
+    }
+
+    function setPostId($postId) {
+        $this->postId = $postId;
+    }
+
+    function setCreation_date($creation_date) {
+        $this->creation_date = $creation_date;
+    }
+
+    function setContent($text) {
+        $this->text = $text;
+    }
+
+    static public function loadCommentById(PDO $conn,$id){
+        $stmt = $conn->prepare('SELECT * FROM comment WHERE id=:id');
+        $result = $stmt->execute(['id' => $id]);
+        
+        if($result == true && $stmt->rowCount() > 0){
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            
+            $loadComment = new comment();
+            $loadComment->id = $row['id'];
+            $loadComment->setPostId($row['post_id']);
+            $loadComment->setUserId($row['user_id']);
+            $loadComment->setCreation_date($row['dateOfCreation']);
+            $loadComment->setContent($row['content']);
+            
+            return $loadComment;
+        }
+        
+        return NULL;
+    }
+
+
 }
--- php/classes/commentTests.php
+++ php/classes/commentTests.php
@@ -1,1 +1,9 @@
 <?php
+require_once __DIR__.'/../dbConfig.php';
+require_once __DIR__.'/commentClass.php';
+$conn = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS);
+
+//Testy dla loadCommentById
+
+//$testComment = comment::loadCommentById($conn, 1);
+//var_dump($testComment);
