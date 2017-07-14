5573a0d04b40f3d21f05dd3a84f92e06f5b2c5be
sweettirexkiller <piotrjankiewicz02@gmail.com>
2017-07-14 18:31:00 +0200

    finished making of log in site, started making of sign in site

--- html/log_in_sign_in_site/index.php
+++ html/log_in_sign_in_site/index.php
@@ -6,16 +6,36 @@ require_once __DIR__ . '/../renderFunction.php';
 require_once __DIR__ . '/../../php/dbConfig.php';
 $conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
 
-if($_SERVER['REQUEST_METHOD'] == 'GET'){
-    require 'logInTemplate.html';
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    if (isset($_GET['new']) && $_GET['new'] == 1) {
+        header('Location: http://localhost/myOwnSmallTwitter/html/log_in_sign_in_site/signInTemplate.html');
+        exit;
+    } else {
+        require 'logInTemplate.html';
+    }
 }
 
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    
-    
-    
-    
-    header('Location: http://localhost/myOwnSmallTwitter/html/main%20/main.php');
-    exit;
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $email = $_POST['email'];
+    $password = $_POST['pwd'];
+    $showed = false;
+    if (strlen($email) > 0 && strlen($password) > 0) {
+        $allUsers = user::loadAllUsers($conn);
+        foreach ($allUsers as $user) {
+            if ($user->getEmail() == $email && $user->verifyPassword($password)) {
+                $_SESSION['logged_user_id'] = $user->getId(); // logged a user by hand
+                header('Location: http://localhost/myOwnSmallTwitter/html/main%20/main.php');
+                exit;
+            }
+        }
+        $data = ['info' => 'Wrong password or email.'];
+        echo render('wrongEmailTemplate.html', $data);
+        $showed = true;
+    } else {
+        if (!$showed) {
+            $data = ['info' => 'Wrong password or email.'];
+            echo render('wrongEmailTemplate.html', $data);
+        }
+    }
 }
 
