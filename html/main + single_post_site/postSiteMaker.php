13f9e40093d63d6a9e617498ba63b0d632943909
sweettirexkiller <piotrjankiewicz02@gmail.com>
2017-07-14 11:58:28 +0200

    added comments under posts, TODO form to add a comment

--- html/main + single_post_site/postSiteMaker.php
+++ html/main + single_post_site/postSiteMaker.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__.'/../../php/classes/postClass.php';
+require_once __DIR__.'/../../php/classes/commentClass.php';
 require_once __DIR__.'/../renderFunction.php';
 require_once __DIR__.'/../../php/dbConfig.php';
 
@@ -8,13 +9,28 @@ if($_SERVER['REQUEST_METHOD'] === 'GET'){
         
         $conn = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS);
         
+        
+        
         $postId = $_GET['id'];
         $singlePostObject = post::loadPostById($conn, $postId);
+        
+        $allPostCommentsOrderedByTime = comment::loadAllCommentsByPostIdOrderedByTimeWithAuthorEmail($conn, $postId);
+        //rendering comments
+        $allCommentsRendered = '';
+        foreach($allPostCommentsOrderedByTime as $comment){
+            $data = [
+                'author_email' => $comment->author,
+                'content' => $comment->getContent(),
+                'dateOfCreation' => $comment->getCreation_date()
+            ];
+            $allCommentsRendered .= render('commentsUnderPostTemplate.html', $data); 
+        }
         $data = [
             'post_id' => $singlePostObject->getId(),
             'user_id' => $singlePostObject->getUser_id(),
             'content' => $singlePostObject->getContent(),
-            'date' => $singlePostObject->getDateOfCreation()
+            'date' => $singlePostObject->getDateOfCreation(),
+            'comments' => $allCommentsRendered
         ];
                 
         echo render('singlePostSiteTemplate.html', $data);
