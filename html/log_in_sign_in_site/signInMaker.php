adf7739038e6cd48c97d80c22d6aa5deb1305c54
sweettirexkiller <piotrjankiewicz02@gmail.com>
2017-07-15 16:16:46 +0200

    finished logging in

--- html/log_in_sign_in_site/signInMaker.php
+++ html/log_in_sign_in_site/signInMaker.php
@@ -1,2 +1,20 @@
 <?php
-echo "I am here";
+session_start();
+require_once __DIR__ . '/../../php/classes/userClass.php';
+require_once __DIR__ . '/../renderFunction.php';
+require_once __DIR__ . '/../../php/dbConfig.php';
+$conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+    if(strlen($_POST['pwd']) > 0 && strlen($_POST['email']) > 1){
+        if(strlen($_POST['pwd']) > 7){
+            $addUser = new user();
+            $addUser->setEmail($_POST['email'])->setPassword($_POST['pwd'])->saveToDB($conn);
+            $_SESSION['logged_user_id'] = $addUser->getId();
+            header('Location: http://localhost/myOwnSmallTwitter/html/main%20/main.php');
+            exit;
+        }else{
+            $data = ['info' => 'Too short password.'];
+            echo render('wrongPasswordTemplate.html', $data);
+        }
+    }
+}
