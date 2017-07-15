f82535c3029718b3ca97079f7b2a2dc51d4f9d76
sweettirexkiller <piotrjankiewicz02@gmail.com>
2017-07-15 19:03:48 +0200

    added site, when you are not logged in and try to go to profile cite

--- html/user_site/userSiteMaker.php
+++ html/user_site/userSiteMaker.php
@@ -1,1 +1,15 @@
 <?php
+session_start();
+require_once __DIR__ . '/../../php/classes/userClass.php';
+require_once __DIR__ . '/../renderFunction.php';
+require_once __DIR__ . '/../../php/dbConfig.php';
+
+$conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
+if($_SERVER['REQUEST_METHOD'] == 'GET'){
+    if(isset($_SESSION['logged_user_id'])){
+        
+    }else{
+        $html = file_get_contents('notLoggedInWarning.html');
+        echo $html;
+    }
+}
